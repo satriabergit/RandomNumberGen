8610fea9014110650d2aaf35093d1cebced27c0a
Pangestu Widodo <pangestu@ittelkom-sby.ac.id>
2021-11-01 15:00:22 +0700

    change main class for using randomnumbergen

--- src/com/PngEngineering/Main.java
+++ src/com/PngEngineering/Main.java
@@ -3,6 +3,6 @@ package com.PngEngineering;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello");
+	    System.out.println("The number i: " + new RandomNumberGen().getRandomNumber());
     }
 }
